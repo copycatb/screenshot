c2c5e92df6e56fa3a7fc15213615a1291004fbee
copycatb <101977997+copycatb@users.noreply.github.com>
2022-03-20 22:05:36 +0530

    Update messages.py

--- bot/messages.py
+++ bot/messages.py
@@ -20,7 +20,7 @@ class Messages:
     )
     PROCESS_UPLOAD_CONFIRM = (
         "Successfully completed process in {total_process_duration}\n\n"
-        "If You find me helpful, please rate me [here](tg://resolve?domain=botsarchive&post=1206)."
+        "If You find me helpful, please join my channel me [here](https://t.me/kashir_bots)."
     )
     WRONG_FORMAT = "Please follow the specified format"
     VIDEO_PROCESS_CAPTION = "Sample video. {duration}s from {start}"
